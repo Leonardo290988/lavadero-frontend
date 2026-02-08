bb8cd19f060f7232080cd1342929312f97eca1d1
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-08 17:54:46 -0300

    arreglo

--- src/utils/fechas.js
+++ src/utils/fechas.js
@@ -1,13 +1,6 @@
 export function formatearFechaHora(fecha) {
   if (!fecha) return "";
 
-  return new Date(fecha).toLocaleString("es-AR", {
-    timeZone: "America/Argentina/Buenos_Aires",
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  });
+
+  
 }
