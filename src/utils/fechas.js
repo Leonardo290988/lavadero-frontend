fa98515cc3ab47585d168be620c04a972b35b22d
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-07 21:06:10 -0300

    arreglo hora

--- src/utils/fechas.js
+++ src/utils/fechas.js
@@ -1,16 +1,10 @@
 export function formatearFecha(fecha) {
   if (!fecha) return "-";
 
-  // fuerza interpretación local SIN UTC
-  const [datePart, timePart] = fecha.split("T");
-  const [year, month, day] = datePart.split("-").map(Number);
-  const [hour, minute, second] = timePart
-    .split(":")
-    .map(v => parseInt(v, 10));
+  // fecha viene como "2026-02-08T00:02:52.000"
+  const [date, time] = fecha.split("T");
+  const [year, month, day] = date.split("-");
+  const [hour, minute, second] = time.split(":");
 
-  const d = new Date(year, month - 1, day, hour, minute, second);
-
-  return d.toLocaleString("es-AR", {
-    hour12: false
-  });
+  return `${day}/${month}/${year}, ${hour}:${minute}:${second.slice(0,2)}`;
 }
