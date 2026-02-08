f0ea87c63a85a044d7932c981d6cf609c9c06006
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-07 21:38:12 -0300

    arreglo definitivo

--- src/utils/fechas.js
+++ src/utils/fechas.js
@@ -1,27 +1,7 @@
 export function formatearFecha(fecha) {
-  if (!fecha || typeof fecha !== "string") return "-";
+  if (!fecha) return "-";
 
-  // Espera: "YYYY-MM-DD HH:mm:ss"
-  const partes = fecha.split(" ");
-  if (partes.length !== 2) return fecha;
-
-  const [datePart, timePart] = partes;
-  const fechaNums = datePart.split("-");
-  const horaNums = timePart.split(":");
-
-  if (fechaNums.length !== 3 || horaNums.length < 2) return fecha;
-
-  const [year, month, day] = fechaNums.map(Number);
-  const [hour, minute, second = 0] = horaNums.map(Number);
-
-  const date = new Date(
-    year,
-    month - 1,
-    day,
-    hour,
-    minute,
-    second
-  );
+  const date = new Date(fecha);
 
   return date.toLocaleString("es-AR", {
     hour12: false
