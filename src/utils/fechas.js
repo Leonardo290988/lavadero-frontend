ff7f7f88c851a5fbb8cb397e5221896a27602026
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-07 21:43:15 -0300

    arreglo

--- src/utils/fechas.js
+++ src/utils/fechas.js
@@ -1,9 +1,15 @@
 export function formatearFecha(fecha) {
   if (!fecha) return "-";
 
-  const date = new Date(fecha);
+  const d = new Date(fecha);
 
-  return date.toLocaleString("es-AR", {
-    hour12: false
-  });
+  const dia = d.getDate().toString().padStart(2, "0");
+  const mes = (d.getMonth() + 1).toString().padStart(2, "0");
+  const anio = d.getFullYear();
+
+  const horas = d.getHours().toString().padStart(2, "0");
+  const minutos = d.getMinutes().toString().padStart(2, "0");
+  const segundos = d.getSeconds().toString().padStart(2, "0");
+
+  return `${dia}/${mes}/${anio}, ${horas}:${minutos}:${segundos}`;
 }
