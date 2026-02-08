81f4f02276e7c67406edd96c8238aa1cabc09fd5
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-08 17:51:06 -0300

    arreglo

--- src/utils/fechas.js
+++ src/utils/fechas.js
@@ -6,5 +6,8 @@ export function formatearFechaHora(fecha) {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
   });
 }
