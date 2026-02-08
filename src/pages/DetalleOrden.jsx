dc4c203474ede6bb1c12c28dbdfff046dbf7f450
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-08 18:19:33 -0300

    hora corregida

--- src/pages/DetalleOrden.jsx
+++ src/pages/DetalleOrden.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { formatearFechaHoraISO } from "../utils/fechas";
 
 
 
@@ -150,7 +151,7 @@ const confirmarOrden = async () => {
         <p><b>Estado:</b> {orden.estado}</p>
         <p>
   <b>Ingreso:</b>{" "}
-  {orden.fecha_ingreso}
+  {formatearFechaHoraISO(orden.fecha_ingreso)}
 </p>
 
         <div className="mt-3 flex items-center gap-3">
--- src/utils/fechas.js
+++ src/utils/fechas.js
@@ -1,6 +1,9 @@
-export function formatearFechaHora(fecha) {
-  if (!fecha) return "";
+export function formatearFechaHoraISO(fechaISO) {
+  if (!fechaISO) return "";
 
+  // "2026-02-08T18:07:58.464Z"
+  const [fecha, hora] = fechaISO.split("T");
+  const [year, month, day] = fecha.split("-");
 
-  
+  return `${day}/${month}/${year} ${hora.substring(0, 8)}`;
 }
