aa4a00c6eae649c10a8db9e29b688e78f56d2519
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-18 11:27:39 -0300

    arreglo hora fronted

--- src/utils/fechas.js
+++ src/utils/fechas.js
@@ -1,9 +1,13 @@
-export function formatearFechaHoraISO(fechaISO) {
-  if (!fechaISO) return "";
+export const formatearFechaHoraISO = (fecha) => {
+  if (!fecha) return "-";
 
-  // "2026-02-08T18:07:58.464Z"
-  const [fecha, hora] = fechaISO.split("T");
-  const [year, month, day] = fecha.split("-");
-
-  return `${day}/${month}/${year} ${hora.substring(0, 8)}`;
-}
+  return new Date(fecha).toLocaleString("es-AR", {
+    timeZone: "America/Argentina/Buenos_Aires",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+};
